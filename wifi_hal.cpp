d919889a135c636281febe705e436ad6ac467526
Pragya Gupta <pragya.gupta@12.36.155.73>
2016-07-20 21:41:47 +0900

    [7570] wlbt: Revert changes for Roam BSSID Blacklist
    This reverts commit ef9baaa57ab6cc6c4108e624937bea58c0cec836
    Since this is a dual band feature , this will be pushed
    into dual band branch .
    
    SCSC-Bug-Id: SSB-19140
    
    Change-Id: If4bbb73e1ea08bf0c525839af67bc0ac768eccd8
    Signed-off-by: Pragya Gupta <pragya.gupta@samsung.com>

--- wifi_hal.cpp
+++ wifi_hal.cpp
@@ -111,7 +111,6 @@ wifi_error init_wifi_vendor_hal_func_table(wifi_hal_fn *fn)
     fn->wifi_set_nodfs_flag = wifi_set_nodfs_flag;
     fn->wifi_start_sending_offloaded_packet = wifi_start_sending_offloaded_packet;
     fn->wifi_stop_sending_offloaded_packet = wifi_stop_sending_offloaded_packet;
-    fn->wifi_set_bssid_blacklist = wifi_set_bssid_blacklist;
     return WIFI_SUCCESS;
 }
 
